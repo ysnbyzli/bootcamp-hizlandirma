530bf3b57870da25d7b7127803ae0c9c43fe2920
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2021-12-18 15:01:51 +0300

    node modul

---
+++ node/03/app.js
@@ -0,0 +1,4 @@
+const { circleCircumference, circleArea } = require("./circle");
+
+console.log(circleArea(12));
+console.log(circleCircumference(12));
---
+++ node/03/circle.js
@@ -0,0 +1,12 @@
+const circleArea = (r) => {
+  return 3.14 * r ** 2;
+};
+
+const circleCircumference = (r) => {
+  return 2 * 3.14 * r;
+};
+
+module.exports = {
+  circleArea,
+  circleCircumference,
+};
